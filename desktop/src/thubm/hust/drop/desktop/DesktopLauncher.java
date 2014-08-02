4c98363649f4e3dbd953c7e258834137aabb4a0f
Bui Minh Thu <buiminhthuk55@gmail.com>
2014-08-03 00:02:52 +0700

    Create game drop

---
+++ desktop/src/thubm/hust/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package thubm.hust.drop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import thubm.hust.drop.GameDrop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameDrop(), config);
+	}
+}
---
+++ html/src/thubm/hust/drop/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package thubm.hust.drop.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import thubm.hust.drop.GameDrop;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new GameDrop();
+        }
+}
