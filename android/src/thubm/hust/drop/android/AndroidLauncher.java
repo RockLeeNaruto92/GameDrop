914c762e62bcb2e65072961930d1f619105ac8c3
Bui Minh Thu <buiminhthuk55@gmail.com>
2014-08-03 00:18:31 +0700

    full game drop source example

--- android/src/thubm/hust/drop/android/AndroidLauncher.java
+++ android/src/thubm/hust/drop/android/AndroidLauncher.java
@@ -8,9 +8,12 @@ import thubm.hust.drop.GameDrop;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		
 		initialize(new GameDrop(), config);
 	}
 }
--- desktop/src/thubm/hust/drop/desktop/DesktopLauncher.java
+++ desktop/src/thubm/hust/drop/desktop/DesktopLauncher.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import thubm.hust.drop.GameDrop;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Game Drop";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new GameDrop(), config);
 	}
 }
